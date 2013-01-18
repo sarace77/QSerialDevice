a47aa7d1a9c6a43ffa30b74d471e085fecba7c91
sarace77 <sarace@email.it>
2013-01-18 15:50:52 +0100

    fixed cursor position

--- QSerialConsole/consolewidget.cpp
+++ QSerialConsole/consolewidget.cpp
@@ -6,9 +6,15 @@
 ConsoleWidget::ConsoleWidget(QWidget *parent) : QTextEdit(parent) {
     setGeometry(5, 5, parent->width() - 10, parent->height() - 10);
     setStyleSheet("ConsoleWidget { background-color:#000000; color: #ffffff}");
+    _lastLine = textCursor();
+}
+
+ConsoleWidget::~ConsoleWidget() {
+
 }
 
 void ConsoleWidget::keyPressEvent(QKeyEvent *event) {
+    setTextCursor(_lastLine);
     setTextColor(QColor(Qt::green));
     if (event->key() == Qt::Key_Enter || event->key() == Qt::Key_Return) {
         QByteArray data = toPlainText().toAscii(), line;
@@ -19,16 +25,22 @@ void ConsoleWidget::keyPressEvent(QKeyEvent *event) {
         }
         emit newCmdSent(line);
     }
-    QTextEdit::keyPressEvent(event);
+    if (event->key() != Qt::Key_Up)
+        QTextEdit::keyPressEvent(event);
+    _lastLine = textCursor();
 }
 
 void ConsoleWidget::printBanner(QString banner) {
+    setTextCursor(_lastLine);
     clear();
     setTextColor(QColor(Qt::white));
     append(banner);
+    _lastLine = textCursor();
 }
 
 void ConsoleWidget::printIncomingMessage(QByteArray msg) {
+    setTextCursor(_lastLine);
     setTextColor(QColor(Qt::yellow));
     append(QString(msg)+QString("\n"));
+    _lastLine = textCursor();
 }
--- QSerialConsole/consolewidget.h
+++ QSerialConsole/consolewidget.h
@@ -2,18 +2,24 @@
 #define CONSOLEWIDGET_H
 
 #include <QKeyEvent>
+#include <QTextCursor>
 #include <QTextEdit>
+
 
 class ConsoleWidget : public QTextEdit
 {
     Q_OBJECT
+
+private:
+    QTextCursor _lastLine;
 
 protected:
     void keyPressEvent(QKeyEvent *event);
 
 public:
     explicit ConsoleWidget(QWidget *parent = 0);   
-    
+    ~ConsoleWidget();
+
 public slots:
     void printBanner(QString banner);
     void printIncomingMessage(QByteArray msg);
